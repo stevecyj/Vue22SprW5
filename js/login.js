26b0b42a420752be59599bdee6c9b013f4f00fc7
root <root@localhost.localdomain>
2022-02-02 13:39:38 +0800

    1st commit

---
+++ js/login.js
@@ -0,0 +1,32 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js'
+
+const site = 'https://vue3-course-api.hexschool.io/v2'
+
+const app = createApp({
+  data() {
+    // 這裡務必使用 function return
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    login() {
+      const url = `${site}/admin/signin`
+      axios
+        .post(url, this.user)
+        .then((res) => {
+          console.log(res.data)
+        })
+        .catch((err) => {
+          console.log(err.response)
+        })
+    },
+  },
+  mounted() {},
+})
+
+// 使用 createApp 建立 Vue 應用，接下來使用 mount 掛載
+app.mount('#app')
