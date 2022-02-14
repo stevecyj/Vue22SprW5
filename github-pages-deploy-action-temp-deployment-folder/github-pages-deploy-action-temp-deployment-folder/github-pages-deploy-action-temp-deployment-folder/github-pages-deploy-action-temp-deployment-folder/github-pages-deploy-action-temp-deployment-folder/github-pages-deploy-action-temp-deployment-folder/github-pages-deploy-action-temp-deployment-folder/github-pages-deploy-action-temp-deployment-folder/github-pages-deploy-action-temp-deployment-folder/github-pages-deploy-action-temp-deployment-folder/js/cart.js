3a900d6ee39b47cbd64fea1c2a77c530e84711d7
stevecyj <ycsteven.tsao@gmail.com>
2022-02-14 05:43:14 +0000

    Deploying to gh-pages from @ stevecyj/Vue22SprW5@6d5567660eec8d5494e9542ab12bde40df442944 🚀

--- github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/js/cart.js
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/js/cart.js
@@ -1,13 +1,32 @@
+/* global axios */
+// eslint-disable-next-line
 import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js';
 
-const site = 'https://vue3-course-api.hexschool.io/v2';
-const api_path = 'steve-vue';
+const site = 'https://vue3-course-api.hexschool.io';
+const apiPath = 'steve-vue';
 
 const app = createApp({
   data() {
     return {
       cartData: {},
+      products: [],
     };
+  },
+  methods: {
+    getProducts() {
+      const apiUrl = `/v2/api/${apiPath}/products/all`;
+      axios
+        .get(`${site}${apiUrl}`, {})
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.error(err.response);
+        });
+    },
+  },
+  mounted() {
+    this.getProducts();
   },
 });
 
